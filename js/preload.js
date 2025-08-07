2ae5280ff2fe7004794d7132b0451fb5e20e3e01
katekdon <katekdon@mail.ru>
2025-08-07 14:58:37 +0300

    Create preload.js

---
+++ js/preload.js
@@ -0,0 +1,5 @@
+function preload() {
+  this.load.image('worm', 'assets/sprites/worm.png');
+  this.load.image('bullet', 'assets/sprites/bullet.png');
+  this.load.image('terrain', 'assets/terrain/ground.png');
+}
